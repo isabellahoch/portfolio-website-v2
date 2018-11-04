31e7768786ec66a764ac106c626a312943b1bb4e
Isabella Hochschild <iehochschild@yahoo.com>
2018-11-04 11:16:21 -0800

    fdsjghk

--- flask/main.py
+++ flask/main.py
@@ -56,6 +56,10 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
