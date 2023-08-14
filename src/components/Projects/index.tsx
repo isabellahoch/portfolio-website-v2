82fbbd0f75f3a3a90e3b435f04d8e738d4b9d313
isabellahoch <40366749+isabellahoch@users.noreply.github.com>
2023-08-13 23:16:33 -0400

    fix projects display

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,10 +1,23 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@mui/styles';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { type RootState } from '../../reducers';
 import { fetchProjects } from '../../actions/portfolioActions';
-import { type Project } from '../../types';
+import ProjectCard from './ProjectCard';
+import './index.css';
+
+const useStyles = makeStyles({
+  masonryContainer: {
+    display: 'flex',
+    margin: '0 auto',
+    width: '90%',
+  },
+});
 
 const Projects: React.FC = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const projectInfo = useSelector((state: RootState) => state.portfolio.projects);
 
@@ -15,11 +28,22 @@ const Projects: React.FC = () => {
   return (
     <div>
       <h1>Projects</h1>
-      <ul>
-        {projectInfo.map((project: Project) => (
-          <li key={project.id}>{project.title}</li>
-        ))}
-      </ul>
+      <div className={classes.masonryContainer}>
+        <TransitionGroup>
+          <ResponsiveMasonry columnsCountBreakPoints={{
+            350: 1, 750: 2, 900: 3, 1200: 4,
+          }}
+          >
+            <Masonry>
+              {projectInfo.map((project) => (
+                <CSSTransition key={project.id} timeout={500} classNames="fade">
+                  <ProjectCard key={project.id} project={project} />
+                </CSSTransition>
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
+        </TransitionGroup>
+      </div>
     </div>
   );
 };
